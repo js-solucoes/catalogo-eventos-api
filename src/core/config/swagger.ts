873c6e3352916fa7c7eb0110834b45fbea280c3c
Professor Jhonata Sobral <jhonata.sobral@ms.senac.br>
2026-03-18 16:42:58 -0400

    ajusta documentação do swagger

--- src/core/config/swagger.ts
+++ src/core/config/swagger.ts
@@ -6,16 +6,53 @@ const swaggerPath = (...segments: string[]) =>
   path.resolve(__dirname, "..", "docs", "swagger", ...segments);
 
 export function loadSwaggerDocument() {
-  
   const base = YAML.load(swaggerPath("base.yaml"));
   const auth = YAML.load(swaggerPath("auth.yaml"));
   const users = YAML.load(swaggerPath("users.yaml"));
-  const touristPoints = YAML.load(swaggerPath("tourist-points.yaml"));
-  const cities = YAML.load(swaggerPath("cities.yaml"));
   const media = YAML.load(swaggerPath("media.yaml"));
-  const events = YAML.load(swaggerPath("events.yaml"));
 
-  const doc = merge.all([base, auth, users, touristPoints, cities, media, events]);
+  const adminCities = YAML.load(swaggerPath("admin.cities.yaml"));
+  const adminEvents = YAML.load(swaggerPath("admin.events.yaml"));
+  const adminHomeBanners = YAML.load(swaggerPath("admin.home-banners.yaml"));
+  const adminHomeHighlights = YAML.load(
+    swaggerPath("admin.home-highlights.yaml"),
+  );
+  const adminInstitutional = YAML.load(swaggerPath("admin.institutional.yaml"));
+  const adminSocialLinks = YAML.load(swaggerPath("admin.social-links.yaml"));
+  const adminTouristPoints = YAML.load(
+    swaggerPath("admin.tourist-points.yaml"),
+  );
+
+  const publicCities = YAML.load(swaggerPath("public.cities.yaml"));
+  const publicEvents = YAML.load(swaggerPath("public.events.yaml"));
+  const publicHomeContent = YAML.load(swaggerPath("public.home-content.yaml"));
+  const publicInstitutional = YAML.load(
+    swaggerPath("public.institutional.yaml"),
+  );
+  const publicSocialLinks = YAML.load(swaggerPath("public.social-links.yaml"));
+  const publicTouristPoints = YAML.load(
+    swaggerPath("public.tourist-points.yaml"),
+  );
+
+  const doc = merge.all([
+    base,
+    auth,
+    users,
+    media,
+    adminCities,
+    adminEvents,
+    adminHomeBanners,
+    adminHomeHighlights,
+    adminInstitutional,
+    adminSocialLinks,
+    adminTouristPoints,
+    publicCities,
+    publicEvents,
+    publicHomeContent,
+    publicInstitutional,
+    publicSocialLinks,
+    publicTouristPoints,
+  ]);
 
   return doc;
 }
